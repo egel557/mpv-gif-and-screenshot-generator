8ddd03bc0750ea2c41dc4bb2c65293efc48a50b7
egel557 <58113537+egel557@users.noreply.github.com>
2022-07-01 18:19:50 +0800

    Add functionality for automated screenshots every 10 seconds.

--- mpv-gif.lua
+++ mpv-gif.lua
@@ -11,7 +11,9 @@ local msg = require 'mp.msg'
 
 local options = {
     dir = "C:/Program Files/mpv/gifs",
-    rez = 600,
+	screenshots_dir = "C:/Users/%USERNAME%/Desktop",
+    gif_rez = 600,
+	screenshot_rez = 640,
     fps = 15,
 }
 
@@ -30,19 +32,23 @@ end
 
 -- Set this to the filters to pass into ffmpeg's -vf option.
 -- filters="fps=24,scale=320:-1:flags=spline"
-filters=string.format("fps=%s,scale='trunc(ih*dar/2)*2:trunc(ih/2)*2',setsar=1/1,scale=%s:-1:flags=spline", fps, options.rez) --change spline to lanczos depending on preference
+-- scale=iw*min(1\\,min(%s/iw\\,%s/ih)):-1
+filters=string.format("fps=%s,setsar=1/1,scale=iw*min(1\\,min(%s/iw\\,%s/ih)):-1", fps, options.gif_rez / 9 * 16, options.gif_rez) --change spline to lanczos depending on preference
+
+-- filters=string.format("fps=%s,scale='trunc(ih*dar/2)*2:trunc(ih/2)*2',setsar=1/1,scale=%s:-1:flags=spline", fps, options.gif_rez / 9 * 16) --change spline to lanczos depending on preference
 
 -- Setup output directory
 output_directory=string.gsub(options.dir, '\"', '')
 
 start_time = -1
 end_time = -1
-palette="%TEMP%palette.png"
+-- palette="%TEMP%palette.png"
+palette = os.getenv("TEMP") .. os.tmpname() .. "png"
 
 -- The roundabout way has to be used due to a some weird
 -- behavior with %TEMP% on the subtitles= parameter in ffmpeg
 -- on Windows–it needs to be quadruple backslashed
-subs = "C:/Users/%USERNAME%/AppData/Local/Temp/subs.srt"
+-- subs = "C:/Users/%USERNAME%/AppData/Local/Temp/subs.srt"
 
 function make_gif_with_subtitles()
     make_gif_internal(true)
@@ -59,6 +65,120 @@ function table_length(t)
 end
 
 
+function extract_subtitles(empty)
+	local temp = os.getenv("TEMP")
+	local subs_path = temp .. os.tmpname() .. "srt"
+
+
+	if empty then
+		local text = '1\n00:00:01,000 --> 00:00:02,000\n<font size="22"></font>\n'
+				
+		local file = io.open(subs_path, "w")
+		file:write(text)
+		file:close()
+		
+		return subs_path
+	end
+	-- Determine currently active sub track
+
+	local i = 0
+	local tracks_count = mp.get_property_number("track-list/count")
+	local subs_array = {}
+	
+	-- check for subtitle tracks
+
+	while i < tracks_count do
+		local type = mp.get_property(string.format("track-list/%d/type", i))
+		local selected = mp.get_property(string.format("track-list/%d/selected", i))
+
+		-- if it's a sub track, save it
+
+		if type == "sub" then
+			local length = table_length(subs_array)
+			subs_array[length] = selected == "yes"
+		end
+		i = i + 1
+	end
+
+	if table_length(subs_array) > 0 then
+
+		local correct_track = 0
+
+		-- iterate through saved subtitle tracks until the correct one is found
+
+		for index, is_selected in pairs(subs_array) do
+			if (is_selected) then
+				correct_track = index
+			end
+		end
+				
+		local path = mp.get_property("path", "")
+		
+		local args = string.format('ffmpeg -i "%s" -map "0:%s" "%s"', esc(path), correct_track + 2, subs_path)
+		msg.debug(args)
+		os.execute(args)
+		
+		return subs_path
+	end
+	
+	return nil
+end
+
+
+function get_subtitle_track()
+	-- Determine currently active sub track
+
+	local i = 0
+	local tracks_count = mp.get_property_number("track-list/count")
+	local subs_array = {}
+	
+	-- check for subtitle tracks
+
+	while i < tracks_count do
+		local type = mp.get_property(string.format("track-list/%d/type", i))
+		local selected = mp.get_property(string.format("track-list/%d/selected", i))
+
+		-- if it's a sub track, save it
+
+		if type == "sub" then
+			local length = table_length(subs_array)
+			subs_array[length] = selected == "yes"
+		end
+		i = i + 1
+	end
+
+	if table_length(subs_array) > 0 then
+
+		local correct_track = 0
+
+		-- iterate through saved subtitle tracks until the correct one is found
+
+		for index, is_selected in pairs(subs_array) do
+			if (is_selected) then
+				correct_track = index
+			end
+		end
+
+		return correct_track
+	end
+	return nil
+end
+
+-- shell escape
+function esc(s)
+	return string.gsub(s, '"', '"\\""')
+end
+
+function esc_for_sub(s)
+	s = string.gsub(s, [[\]], [[/]])
+	s = string.gsub(s, '"', '"\\""')
+	s = string.gsub(s, ":", [[\\:]])
+	s = string.gsub(s, "'", [[\\']])
+	s = string.gsub(s, "%[", "\\%[")
+	s = string.gsub(s, "%]", "\\%]")
+	return s
+end
+
 function make_gif_internal(burn_subtitles)
     local start_time_l = start_time
     local end_time_l = end_time
@@ -69,70 +189,36 @@ function make_gif_internal(burn_subtitles)
 
     mp.osd_message("Creating GIF.")
 
-    -- shell escape
-    function esc(s)
-        return string.gsub(s, '"', '"\\""')
-    end
+    -- -- shell escape
+    -- function esc(s)
+        -- return string.gsub(s, '"', '"\\""')
+    -- end
 
-    function esc_for_sub(s)
-        s = string.gsub(s, [[\]], [[/]])
-        s = string.gsub(s, '"', '"\\""')
-        s = string.gsub(s, ":", [[\\:]])
-        s = string.gsub(s, "'", [[\\']])
-        return s
-    end
+    -- function esc_for_sub(s)
+        -- s = string.gsub(s, [[\]], [[/]])
+        -- s = string.gsub(s, '"', '"\\""')
+        -- s = string.gsub(s, ":", [[\\:]])
+        -- s = string.gsub(s, "'", [[\\']])
+		-- s = string.gsub(s, "%[", "\\%[")
+		-- s = string.gsub(s, "%]", "\\%]")
+        -- return s
+    -- end
 
     local pathname = mp.get_property("path", "")
     local trim_filters = esc(filters)
 
     local position = start_time_l
     local duration = end_time_l - start_time_l
+	local subs_path = extract_subtitles(not burn_subtitles)
 
-    if burn_subtitles then
-        -- Determine currently active sub track
-
-        local i = 0
-        local tracks_count = mp.get_property_number("track-list/count")
-        local subs_array = {}
-        
-        -- check for subtitle tracks
-
-        while i < tracks_count do
-            local type = mp.get_property(string.format("track-list/%d/type", i))
-            local selected = mp.get_property(string.format("track-list/%d/selected", i))
-
-            -- if it's a sub track, save it
-
-            if type == "sub" then
-                local length = table_length(subs_array)
-                subs_array[length] = selected == "yes"
-            end
-            i = i + 1
-        end
-
-        if table_length(subs_array) > 0 then
-
-            local correct_track = 0
-
-            -- iterate through saved subtitle tracks until the correct one is found
-
-            for index, is_selected in pairs(subs_array) do
-                if (is_selected) then
-                    correct_track = index
-                end
-            end
-
-            trim_filters = trim_filters .. string.format(",subtitles=%s:si=%s", esc_for_sub(pathname), correct_track)
-
-        end
-
-    end
+	trim_filters = trim_filters .. string.format(",subtitles=%s", esc_for_sub(subs_path))
 
 
     -- first, create the palette
     args = string.format('ffmpeg -v warning -ss %s -t %s -i "%s" -vf "%s,palettegen" -y "%s"', position, duration, esc(pathname), esc(trim_filters), esc(palette))
     msg.debug(args)
     os.execute(args)
+	
 
     -- then, make the gif
     local filename = mp.get_property("filename/no-ext")
@@ -159,6 +245,11 @@ function make_gif_internal(burn_subtitles)
 
     args = string.format('ffmpeg -v warning -ss %s %s -t %s -i "%s" -i "%s" -lavfi "%s [x]; [x][1:v] paletteuse" -y "%s"', position, copyts, duration, esc(pathname), esc(palette), esc(trim_filters), esc(gifname))
     os.execute(args)
+	os.remove(palette)
+	
+	if subs_path ~= nil then
+		os.remove(subs_path)
+	end
 
     local ok, err, code = os.rename(gifname, gifname)
 	if ok then
@@ -167,6 +258,53 @@ function make_gif_internal(burn_subtitles)
 	else
 	    mp.osd_message("Error creating file, check CLI for more info.")
 	end
+end
+
+function make_screenshots_internal(burn_subtitles)
+	mp.osd_message("Taking screenshots...")
+			
+	local pathname = mp.get_property("path", "")
+	local filename = mp.get_property("filename", "")
+	-- local dir = pathname:match("(.*[/\\])")
+	-- local dir = string.gsub(pathname, "\\" .. filename, "")
+	local folder = string.format('%s_%s', filename, os.time(os.date("!*t")))
+	local subs_path = extract_subtitles(not burn_subtitles)
+	
+	
+	print(subs_path)
+	-- options.gif_rez / 9 * 16, options.gif_rez
+	local filters = string.format("-vf scale=iw*min(1\\,min(%s/iw\\,%s/ih)):-1", options.screenshot_rez / 9 * 16, options.screenshot_rez)
+	
+	filters = filters .. string.format(",subtitles=%s", esc_for_sub(subs_path))
+
+
+	
+	os.execute(string.format('cd %s & mkdir "%s"', options.screenshots_dir, folder))
+	
+	local fps = math.ceil(mp.get_property("estimated-vf-fps", ""))
+	
+	filters = filters .. string.format(",select='not(mod(n,%s*10))',setpts='N/(%s*TB)'", 24, 24)
+
+	
+	local args = string.format('ffmpeg -i "%s" %s -f image2 "%s/%s/%%03d.png"', esc(pathname), filters, options.screenshots_dir, esc(folder))
+	-- args = string.format('ffmpeg -i "%s" -r 0.1 %s "%s/%s/%%03d.png"', esc(pathname), filters, options.screenshots_dir, esc(folder))
+	msg.debug(args)
+	print(args)
+	os.execute(args)	
+	
+	if subs_path ~= nil then
+		os.remove(subs_path)
+	end
+
+	
+end
+
+function make_screenshots()
+	make_screenshots_internal(false)
+end
+
+function make_screenshots_with_subtitles()
+	make_screenshots_internal(true)
 end
 
 function set_gif_start()
@@ -184,7 +322,17 @@ function file_exists(name)
     if f~=nil then io.close(f) return true else return false end
 end
 
+
+function debug_func()
+	print(mp.get_property("estimated-vf-fps", ""))
+end
+
+
 mp.add_key_binding("g", "set_gif_start", set_gif_start)
 mp.add_key_binding("G", "set_gif_end", set_gif_end)
 mp.add_key_binding("Ctrl+g", "make_gif", make_gif)
 mp.add_key_binding("Ctrl+G", "make_gif_with_subtitles", make_gif_with_subtitles)
+mp.add_key_binding("Alt+s", "make_screenshots", make_screenshots)
+mp.add_key_binding("Alt+S", "make_screenshots_with_subtitles", make_screenshots_with_subtitles)
+
+mp.add_key_binding("Tab", "debug_func", debug_func)
